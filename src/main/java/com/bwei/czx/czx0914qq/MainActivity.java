95329e941eb517b7a90ef84652f5a028680b1d58
chengzexiang <1154618994@qq.com>
2017-09-19 17:14:17 +0800

    QQ第三方登录

---
+++ src/main/java/com/bwei/czx/czx0914qq/MainActivity.java
@@ -0,0 +1,137 @@
+package com.bwei.czx.czx0914qq;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.auth.QQToken;
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String APP_ID = "1105602574";//官方获取的APPID
+    private Tencent mTencent;
+    private BaseUiListener mIUiListener;
+    private UserInfo mUserInfo;
+    private Button login;
+    private TextView name;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //传入参数APPID和全局Context上下文
+        mTencent = Tencent.createInstance(APP_ID, MainActivity.this.getApplicationContext());
+
+        initView();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIUiListener = new BaseUiListener();
+                //all表示获取所有权限
+                mTencent.login(MainActivity.this,"all", mIUiListener);
+                mUserInfo = new UserInfo(MainActivity.this, mTencent.getQQToken()); //获取用户信息
+                mUserInfo.getUserInfo(mIUiListener);
+            }
+        });
+    }
+
+    private void initView() {
+        login = (Button) findViewById(R.id.login);
+        name = (TextView) findViewById(R.id.name);
+        img = (ImageView) findViewById(R.id.img);
+    }
+    /**
+     * 自定义监听器实现IUiListener接口后，需要实现的3个方法
+     * onComplete完成 onError错误 onCancel取消
+     */
+    private class BaseUiListener implements IUiListener {
+
+        @Override
+        public void onComplete(Object response) {
+            Toast.makeText(MainActivity.this, "授权成功", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "response:" + response);
+            JSONObject obj = (JSONObject) response;
+            try {
+                String openID = obj.getString("openid");
+                String accessToken = obj.getString("access_token");
+                String expires = obj.getString("expires_in");
+                mTencent.setOpenId(openID);
+                mTencent.setAccessToken(accessToken,expires);
+                QQToken qqToken = mTencent.getQQToken();
+                mUserInfo = new UserInfo(getApplicationContext(),qqToken);
+                mUserInfo.getUserInfo(new IUiListener() {
+                    @Override
+                    public void onComplete(Object response) {
+                        Log.e(TAG,"登录成功"+response.toString());
+                        if(response == null){
+                            return;
+                        }
+                        try {
+                            JSONObject jo = (JSONObject) response;
+                                Toast.makeText(MainActivity.this, "登录成功",
+                                        Toast.LENGTH_LONG).show();
+                            String nickName = jo.getString("nickname");
+                            String  figureurl_1= jo.getString("figureurl_1");
+                            name.setText(nickName);
+                            ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(MainActivity.this).build();
+                            ImageLoader.getInstance().init(configuration);
+                            ImageLoader.getInstance().displayImage(figureurl_1,img);
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+                    }
+
+                    @Override
+                    public void onError(UiError uiError) {
+                        Log.e(TAG,"登录失败"+uiError.toString());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.e(TAG,"登录取消");
+
+                    }
+                });
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+            Toast.makeText(MainActivity.this, "授权失败", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(MainActivity.this, "授权取消", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == Constants.REQUEST_LOGIN){
+            Tencent.onActivityResultData(requestCode,resultCode,data,mIUiListener);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
